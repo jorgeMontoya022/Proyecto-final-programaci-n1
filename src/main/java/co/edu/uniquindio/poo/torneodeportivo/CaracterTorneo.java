e939c62e1919b1b5e29e46dfc37a86680c32eaf2
jorgemontoya022 <jorgew.montoyat@uqvirtual.edu.co>
2023-11-09 14:53:34 -0500

    se agrega la clase juez

--- src/main/java/co/edu/uniquindio/poo/torneodeportivo/CaracterTorneo.java
+++ src/main/java/co/edu/uniquindio/poo/torneodeportivo/CaracterTorneo.java
@@ -2,11 +2,11 @@ package co.edu.uniquindio.poo.torneodeportivo;
 
 public enum CaracterTorneo {
     INDIVIDUAL{
-        public boolean esValido(Participante participante){
+        public boolean esValido(Participante participante) {
             return participante instanceof Jugador;
         }
     },GRUPAL{
-        public boolean esValido(Participante participante){
+        public boolean esValido(Participante participante) {
             return participante instanceof Equipo;
         }
     };
